7acfc2ae647b2c177be8bdcdb6e30d45d26029c5
Jaspal Singh <jsinghw94@gmail.com>
2020-06-24 19:20:06 -0400

    basic homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super (props)
+        this.state = {
+            posts: [],
+        }
+    }
+    componentDidMount() {
+        fetch('http://127.0.0.1:8000/api/post/?format=json')
+        .then(res => res.json())
+        .then(data => this.setState({posts: data}))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Homepage</h1>
+
+                {this.state.posts.map((p) => {
+                    return (
+                        <div>
+                            <h2>{p.boast_or_roast === 'B' ? 'Boast' : 'Roast'}</h2>
+                            <ul>
+                                <li>{p.content}</li>
+                                <li>upvotes: {p.upvotes} | downvotes: {p.downvotes}</li>
+                                <li>date created: {p.upload_date}</li>
+                                <li>upvote & downvote buttons here</li>
+                            </ul>
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
